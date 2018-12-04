8aa1737c3785237512a7c8669566dabb2f9556d5
Pavel Gatilov <Pavel_Gatilov@epam.com>
2018-12-04 11:45:56 +0300

    Published latest changes to the sample

--- Authentication.sh
+++ Authentication.sh
@@ -7,12 +7,12 @@ Authentication_File='Credentials.txt'
 appkey=$(grep 'App Key' ${Authentication_File} | cut -d : -f2 | tr -d ' ')
 appsecret=$(grep 'App Secret' ${Authentication_File} | cut -d : -f2 | tr -d ' ')
 usersyncplicityapptoken=$(grep 'Application Token' ${Authentication_File} | cut -d : -f2 | tr -d ' ')
-basicAuthToken=$(echo -n "${appkey}:${appsecret}" | base64)
+basicAuthToken=$(echo -n "${appkey}:${appsecret}" | base64 -w 0)
 
 oauthresult=$(curl -sS -X POST https://api.syncplicity.com/oauth/token -H 'Authorization: Basic '${basicAuthToken} \
 -H "Sync-App-Token: ${usersyncplicityapptoken}" -d 'grant_type=client_credentials')
 
-accesstoken=$(echo ${oauthresult} | sed -e 's/[{}"]/''/g' | awk -v RS=',' -F: '/^access_token/ {print $2}')
-companyID=$(echo ${oauthresult} | sed -e 's/[{}"]/''/g' | awk -v RS=',' -F: '/^user_company_id/ {print $2}')
+accesstoken=$(echo ${oauthresult} | jq .access_token | tr -d '"')
+companyID=$(echo ${oauthresult} | jq .user_company_id | tr -d '"')
 
 echo -e "App Key: ${appkey}\nAccess Token: ${accesstoken}\nCompany ID:${companyID}"
